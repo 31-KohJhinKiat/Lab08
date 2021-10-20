40a864a5239dc80adc52680ffa92d57d4304423a
JhinKiat <jhinkiat915@gmail.com>
2021-10-20 19:46:19 +0800

    Added music

---
+++ 31_KohJhinKiat_Lab08/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int scoreValue;
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addScore()
+    {
+        scoreValue ++;
+        ScoreText.GetComponent<Text>().text = "Score: " + scoreValue;
+    }
+
+}
---
+++ 31_KohJhinKiat_Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      if(transform.position.y <= -7 || transform.position.y >= 7)
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+
+    }
+
+    public void OnCollisionEnter(Collision others)
+    {
+        if (others.gameObject.tag.Equals("Obstacle"))
+        {
+            print("Collide");
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+}
